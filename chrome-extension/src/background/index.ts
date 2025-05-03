4110a2ac621a38bb0323bcd53b177968777243f0
fooqoo <horseshoes.buzz.17@gmail.com>
2025-05-03 21:22:15 +0900

    Replace popup with side panel activation

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -5,5 +5,12 @@ exampleThemeStorage.get().then(theme => {
   console.log('theme', theme);
 });
 
+// Add event listener to open side panel when extension icon is clicked
+chrome.action.onClicked.addListener(async tab => {
+  // Open the side panel in the current tab
+  await chrome.sidePanel.open({ tabId: tab.id });
+  console.log('Side panel opened for tab:', tab.id);
+});
+
 console.log('Background loaded');
 console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
